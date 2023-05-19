09180de20496423f91f9c0035069171c382aa4a4
MargaridaTeles <telesmaegarida@gmail.com>
2023-05-19 14:26:10 +0100

    Ordenar players por score, alfabética ascendente e descendente

--- PlayerManager4/CompareByName.cs
+++ PlayerManager4/CompareByName.cs
@@ -5,14 +5,36 @@ namespace PlayerManager4
 {
     public class CompareByName : IComparer<Player>
     {
-
-        public CompareByName(bool ordem)
+        private bool ascendente;
+        private bool score = false;
+        public CompareByName()
         {
-            if(ordem = true)
-            {
-
-            }
+            score = true;
         }
+
+        public CompareByName(bool ascendente)
+        {
+            this.ascendente = ascendente;
+        }
+
+        public int Compare(Player player1, Player player2)
+        {
+            if(score)
+            {
+                return (player2.Score - player1.Score);
+            }
+
+            if(ascendente)
+            {
+                return(string.Compare(player1.Name, player2.Name));
+            }
+            else if(!ascendente)
+            {
+                return(string.Compare(player1.Name, player2.Name) * -1);
+            }
+            return 0;
+        }
+
         
     }
 
