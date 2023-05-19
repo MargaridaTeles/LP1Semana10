79c6a1ddd888892e3db6e09db304dc4871939c9c
MargaridaTeles <telesmaegarida@gmail.com>
2023-05-19 14:45:22 +0100

    Coleções genéricas com ciclo foreach e imprimir as strings de cada uma

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringCollections
 {
@@ -6,7 +7,57 @@ namespace StringCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ICollection<string> lista = new List<string>();
+            Stack<string> stack = new Stack<string>();
+            Queue<string> queue = new Queue<string>();
+            HashSet<string> hashset = new HashSet<string>();
+
+            lista.Add("Banana");
+            lista.Add("Pera");
+            lista.Add("Manga");
+            lista.Add("Banana");
+            lista.Add("Maça");
+
+            stack.Push("Banana");
+            stack.Push("Pera");
+            stack.Push("Manga");
+            stack.Push("Banana");
+            stack.Push("Maça");
+
+            queue.Enqueue("Banana");
+            queue.Enqueue("Pera");
+            queue.Enqueue("Manga");
+            queue.Enqueue("Banana");
+            queue.Enqueue("Maça");
+
+            hashset.Add("Banana");
+            hashset.Add("Pera");
+            hashset.Add("Manga");
+            hashset.Add("Banana");
+            hashset.Add("Maça");
+
+
+            foreach(string i in lista)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Write("\n");
+            foreach(string i in stack)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Write("\n");
+
+            foreach(string i in queue)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Write("\n");
+
+            foreach(string i in hashset)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
