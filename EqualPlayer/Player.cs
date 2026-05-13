9ce1aa947465d80b6fc5daf1b9ce8ea2fd8813eb
MargaridaTeles <telesmaegarida@gmail.com>
2026-05-13 11:44:57 +0100

    Add project EqualPlayer and add new class

---
+++ EqualPlayer/Player.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EqualPlayer
+{
+    public class Player
+    {
+        public PlayerClass PClass { get; }
+        public string Name { get; }
+        public Player(PlayerClass pClass, string name)
+        {
+            PClass = pClass;
+            Name = name;
+        }
+
+        public override int GetHashCode()
+        {
+            return PClass.GetHashCode() ^ Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Player otherPlayer = obj as Player;
+
+            if (otherPlayer is null) 
+                return false;
+
+            if (Name.Equals(otherPlayer.Name) && PClass.Equals(otherPlayer.PClass))
+                return true;
+            
+            return false;
+        }
+    }
+}
