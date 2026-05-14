2a10f6300ca53a626a756a479e4f80876b435ecd
MargaridaTeles <telesmaegarida@gmail.com>
2026-05-14 10:28:59 +0100

    Add new project FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string fileName = args[0];
+            Console.WriteLine("Insere texto (linha vazia para terminar):");
+
+            using StreamWriter writer = new StreamWriter(fileName);
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                    writer.WriteLine(input);
+
+            } while (!string.IsNullOrEmpty(input));
+        }
+    }
+}
